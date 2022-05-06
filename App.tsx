bc17aeb4f98db52182458f5a720f69907a1e170b
Ariun <thiagotatico007@gmail.com>
2022-05-06 15:15:55 -0300

    Changes

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import 'react-native-gesture-handler';
 import { View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { theme } from './src/theme';
-import { Widget } from './src/components/Widget';
+import Widget from './src/components/Widget';
 import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import AppLoading from 'expo-app-loading';
 
--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 import { TouchableOpacity } from 'react-native';
 import { ChatTeardropDots } from 'phosphor-react-native';
 import BottomSheet from '@gorhom/bottom-sheet'
-
 import { styles } from './styles';
 import { theme } from '../../theme';
+import { Options } from '../Options';
 
 
-export function Widget() {
+function Widget() {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  function handleOpen() {
+    bottomSheetRef.current?.expand();
+  }
+
   return (
     <>
       <TouchableOpacity 
         style={styles.button}
+        onPress={handleOpen} 
       >
         <ChatTeardropDots
           size={24}
@@ -19,6 +27,17 @@ export function Widget() {
           color={theme.colors.text_on_brand_color}
         />
       </TouchableOpacity>
+
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={[1, 280]}
+        backgroundStyle={styles.modal}
+        handleIndicatorStyle={styles.indicator}
+      >
+        <Options />
+      </BottomSheet>
     </>
   );
 }
+
+export default gestureHandlerRootHOC(Widget);
