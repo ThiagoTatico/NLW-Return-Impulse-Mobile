792c765a42da7a5fd3fbff8f2a599f220741e94b
Ariun <thiagotatico007@gmail.com>
2022-05-09 09:12:07 -0300

    Changes

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { ArrowLeft } from 'phosphor-react-native';
+import { View, TextInput, Image, Text, TouchableOpacity} from 'react-native';
+import { theme } from '../../theme';
+import { styles } from './styles';
+import { FeedbackType } from '../../components/Widget';
+import { feedbackTypes } from '../../utils/feedbackTypes';
+
+interface Props {
+  feedbackType: FeedbackType;
+}
+
+export function Form({ feedbackType }: Props) {
+  const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <ArrowLeft 
+            size={24}
+            weight="bold"
+            color={theme.colors.text_secondary}
+          />
+        </TouchableOpacity>
+
+        <View style={styles.titleContainer}>
+          <Image
+            source={feedbackTypeInfo.image}
+            style={styles.image}
+          />
+
+          <Text style={styles.titleText}>
+            {feedbackTypeInfo.title}
+          </Text>
+        </View>
+
+      </View>
+
+      <TextInput 
+        multiline
+        style={styles.input}
+        placeholder="Algo não está funcionando bem? Queremos corrigir. Conte com detalhes o que está acontecendo..."
+        placeholderTextColor={theme.colors.text_secondary}
+      />
+
+    </View>
+  );
+}
--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -6,7 +6,10 @@ import BottomSheet from '@gorhom/bottom-sheet'
 import { styles } from './styles';
 import { theme } from '../../theme';
 import { Options } from '../Options';
+import { Form} from '../Form';
+import { feedbackTypes } from '../../utils/feedbackTypes';
 
+export type FeedbackType = keyof typeof feedbackTypes;
 
 function Widget() {
   const bottomSheetRef = useRef<BottomSheet>(null);
@@ -34,7 +37,9 @@ function Widget() {
         backgroundStyle={styles.modal}
         handleIndicatorStyle={styles.indicator}
       >
-        <Options />
+        <Form 
+          feedbackType="BUG"
+        />
       </BottomSheet>
     </>
   );
